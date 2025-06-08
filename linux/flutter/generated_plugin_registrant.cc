e0c99065cf304ac92fe147881e06a09800f1f844
Biz <chebizarro@gmail.com>
2025-06-08 09:29:16 -0700

    refactor: remove system tray dependency and update packages to latest versions

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -7,13 +7,9 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_secure_storage_linux/flutter_secure_storage_linux_plugin.h>
-#include <system_tray/system_tray_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) flutter_secure_storage_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterSecureStorageLinuxPlugin");
   flutter_secure_storage_linux_plugin_register_with_registrar(flutter_secure_storage_linux_registrar);
-  g_autoptr(FlPluginRegistrar) system_tray_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "SystemTrayPlugin");
-  system_tray_plugin_register_with_registrar(system_tray_registrar);
 }
