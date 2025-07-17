ca4c2615386bc69651cc47d1ad232bbb6718c921
Andrea Diaz Correia <andrea.diaz.correia@gmail.com>
2025-07-16 21:55:54 -0300

    feat: add message copy functionality and update Mostro public key

--- lib/features/chat/widgets/message_bubble.dart
+++ lib/features/chat/widgets/message_bubble.dart
@@ -1,6 +1,8 @@
 import 'package:dart_nostr/nostr/model/event/event.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mostro_mobile/core/app_theme.dart';
+import 'package:mostro_mobile/generated/l10n.dart';
 
 class MessageBubble extends StatelessWidget {
   final NostrEvent message;
@@ -19,15 +21,34 @@ class MessageBubble extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       alignment: isFromPeer ? Alignment.centerLeft : Alignment.centerRight,
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          color: isFromPeer ? AppTheme.backgroundCard : AppTheme.purpleAccent,
-          borderRadius: BorderRadius.circular(12),
+      child: GestureDetector(
+        onLongPress: () => _copyToClipboard(context, message.content!),
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: isFromPeer ? AppTheme.backgroundCard : AppTheme.purpleAccent,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Text(
+            message.content!,
+            style: const TextStyle(color: AppTheme.cream1),
+          ),
         ),
-        child: Text(
-          message.content!,
-          style: const TextStyle(color: AppTheme.cream1),
+      ),
+    );
+  }
+
+  void _copyToClipboard(BuildContext context, String text) {
+    Clipboard.setData(ClipboardData(text: text));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(S.of(context)!.messageCopiedToClipboard),
+        duration: const Duration(seconds: 2),
+        backgroundColor: AppTheme.backgroundCard,
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.all(16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
         ),
       ),
     );
