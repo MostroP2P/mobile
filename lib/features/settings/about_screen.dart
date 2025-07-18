d91088d12a2fdf85b4dd617a2dbbed72f8bd9b05
Francisco Calderón <fjcalderon@gmail.com>
2025-07-18 16:36:31 -0300

    Move the license to locales

--- lib/features/settings/about_screen.dart
+++ lib/features/settings/about_screen.dart
@@ -15,28 +15,6 @@ class AboutScreen extends ConsumerWidget {
 
   const AboutScreen({super.key});
 
-  static const String licenseText = '''MIT License
-
-Copyright (c) 2025 Mostro team
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.''';
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final nostrEvent = ref.watch(orderRepositoryProvider).mostroInstance;
@@ -790,7 +768,7 @@ SOFTWARE.''';
                 child: SingleChildScrollView(
                   padding: const EdgeInsets.all(12),
                   child: Text(
-                    licenseText,
+                    S.of(context)!.mitLicenseText.replaceAll('\\n', '\n'),
                     style: const TextStyle(
                       color: Colors.white,
                       fontSize: 14,
