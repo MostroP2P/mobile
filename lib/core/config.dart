62cdce43c7e0e0af5eebbbe35c81b3cc25d2c495
Brian Ramirez <120234671+BraCR10@users.noreply.github.com>
2025-08-26 14:19:45 -0600

    Update mostroPubKey to new value

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -10,7 +10,9 @@ class Config {
   ];
 
   // Mostro hexkey
-  static const String mostroPubKey = '9d9d0455a96871f2dc4289b8312429db2e925f167b37c77bf7b28014be235980';
+  static const String mostroPubKey = 
+          '82fa8cb978b43c79b2156585bac2c011176a21d2aead6d9f7c575c005be88390';
+  //'9d9d0455a96871f2dc4289b8312429db2e925f167b37c77bf7b28014be235980';
 
   static const String dBName = 'mostro.db';
   static const String dBPassword = 'mostro';
